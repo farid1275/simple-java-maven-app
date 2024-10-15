df8d9adf5129d22b3bc776c204f7c09973d7dc08
farid1275 <farid27orbe@gmail.com>
2024-10-15 17:23:51 +0200

    Update javamavenDSL.groovy

--- DSL/javamavenDSL.groovy
+++ DSL/javamavenDSL.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/farid1275/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('farid1275')
+            node / gitConfigEmail('farid27orbe@gmail.com')
         }
     }
     steps {
